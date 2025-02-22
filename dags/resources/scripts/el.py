d5c5bfdb2e9819cfe70fdf144d3dc186baff5bff
fajri-yanti <gitfajri@gmail.com>
2025-02-22 09:25:19 +0700

    Update el.py

--- dags/resources/scripts/el.py
+++ dags/resources/scripts/el.py
@@ -12,21 +12,16 @@ def extract_load(file_path, table_name, schema='raw_data'):
     )
     cur = conn.cursor()
 
-    # Cek & Buat schema jika belum ada
     cur.execute(f"CREATE SCHEMA IF NOT EXISTS {schema};")
 
-    # Gunakan schema
     cur.execute(f"SET search_path TO {schema};")
 
-    # Load CSV
     df = pd.read_csv(file_path)
 
-    # Buat CREATE TABLE jika belum ada
     columns = ', '.join([f'"{col}" TEXT' for col in df.columns])
     create_table_query = f"CREATE TABLE IF NOT EXISTS {table_name} ({columns});"
     cur.execute(create_table_query)
 
-    # Load CSV pakai COPY
     copy_query = f"COPY {table_name} FROM STDIN WITH CSV HEADER DELIMITER ','"
     with open(file_path, 'r') as f:
         cur.copy_expert(copy_query, f)
